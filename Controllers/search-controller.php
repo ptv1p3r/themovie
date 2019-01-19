68f59811975ebb295ff03dd8bdc2db30d1278ca7
Pedro Roldan <pedro.roldan@gmail.com>
2019-01-19 14:15:52 +0000

    initial

---
+++ Controllers/search-controller.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: V1p3r
+ * Date: 17/10/2018
+ * Time: 20:10
+ */
+/**
+ * search - Controller
+ */
+class SearchController
+{
+
+    /**
+     * Carrega a página "/views/search/search-view.php"
+     */
+    public function view() {
+        // Título da página
+        $this->title = 'Search';
+
+        // Parametros da função
+        $parametros = ( func_num_args() >= 1 ) ? func_get_arg(0) : array();
+
+        print_r($parametros);
+
+        // página não precisa de modelo
+        $this->cssFile = ABSPATH . '/css/bootstrap.min.css';
+
+        /** Carrega os arquivos do view **/
+
+        require ABSPATH . '/views/_includes/header.php';
+
+        require ABSPATH . '/views/search/search-view.php';
+
+        require ABSPATH . '/views/_includes/footer.php';
+
+    }
+
+}
---
+++ Views/search/search-view.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: V1p3r
+ * Date: 19/01/2019
+ * Time: 14:09
+ */
+echo "search";
