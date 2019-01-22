a7b02b1cfe1499716970b7f576664da8e611e972
SlyerX <l.moreira20@hotmail.com>
2019-01-22 22:16:57 +0000

    Admin Login form

--- Admin/Views/View.php
+++ Admin/Views/View.php
@@ -5,3 +5,30 @@
  * Date: 17/10/2018
  * Time: 20:14
  */
+
+include "../_includes/header.php"
+?>
+
+<div class="container">
+    <div class="row  justify-content-center align-items-center ">
+        <div class="col-lg-4">
+            <div class="jumbotron" style="margin-top: 300px">
+
+                <form>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Username">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Password">
+                    </div>
+
+                    <button type="submit" class="btn btn-success form-control">Login</button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include "../_includes/footer.php"?>
---
+++ Admin/_includes/footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: V1p3r
+ * Date: 21/11/2018
+ * Time: 22:55
+ */
+?>
+
+<div class="container-fluid">
+    <p class="text-muted text-center">Copyright ISMAT &copy;<?php echo date('Y');?> -
+        SIM - Pedro Roldan, Leandro Moreira, Rubén Lourenço</p>
+</div>
+
+</body>
+</html>
