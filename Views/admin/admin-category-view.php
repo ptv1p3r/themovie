3101b5e999048bcfad6900e0f971dc97b04eef02
SlyerX <l.moreira20@hotmail.com>
2019-01-26 21:31:45 +0000

    toolbar updates

--- Views/admin/admin-category-view.php
+++ Views/admin/admin-category-view.php
@@ -16,19 +16,17 @@
 
     <!-- Navbar -->
     <ul class="navbar-nav ml-auto ml-md-0">
-        <ul class="navbar-nav ml-auto ml-md-0">
-            <li class="nav-item dropdown no-arrow">
-                <a class="nav-link dropdown-toggle" href="" id="userDropdown" role="button"
-                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img src="../../Images/user.png" alt="" height="48">
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                    <a class="dropdown-item" href="<?php echo HOME_URI . '/admin/settings';?>">Settings</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
-                </div>
-            </li>
-        </ul>
+        <li class="nav-item dropdown no-arrow">
+            <a class="nav-link dropdown-toggle" href="" id="userDropdown" role="button"
+               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img src="../../Images/user.png" alt="" height="48">
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                <a class="dropdown-item" href="<?php echo HOME_URI . '/admin/settings';?>">Settings</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+            </div>
+        </li>
     </ul>
 </nav>
 
